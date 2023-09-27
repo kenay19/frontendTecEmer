3f3d54326ba92c8258fbc9c3e915ca3e88677c5a
Kenay19 <omarlazor@hotmail.com>
2023-09-26 21:29:33 -0600

    checado

--- src/app/vendedor/vendedor.module.ts
+++ src/app/vendedor/vendedor.module.ts
@@ -8,6 +8,7 @@ import { VendedorPageRoutingModule } from './vendedor-routing.module';
 
 import { VendedorPage } from './vendedor.page';
 import { ListadoVendedorComponent } from '../components/listado-vendedor/listado-vendedor.component';
+import { FormularioVendedorComponent } from '../components/formulario-vendedor/formulario-vendedor.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -15,6 +16,6 @@ import { ListadoVendedorComponent } from '../components/listado-vendedor/listado
     IonicModule,
     VendedorPageRoutingModule
   ],
-  declarations: [VendedorPage,ListadoVendedorComponent]
+  declarations: [VendedorPage,ListadoVendedorComponent,FormularioVendedorComponent]
 })
 export class VendedorPageModule {}
